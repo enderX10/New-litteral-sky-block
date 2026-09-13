8506654b93a04350ddd406e70044a23c861680b1
enderX10 <enderx.xe@gmail.com>
2026-09-13 15:17:11 +0200

    Delete literal-sky-block-fabric-main directory

--- literal-sky-block-fabric-main/literal-sky-block-fabric-main/src/main/java/io/github/gaming32/literalskyblock/client/LevelRendererLSB.java
+++
@@ -1,7 +0,0 @@
-package io.github.gaming32.literalskyblock.client;
-
-import net.minecraft.client.renderer.LightTexture;
-
-public interface LevelRendererLSB {
-    void renderSnowAndRainLSB(LightTexture arg, float g, double d, double e, double h);
-}
