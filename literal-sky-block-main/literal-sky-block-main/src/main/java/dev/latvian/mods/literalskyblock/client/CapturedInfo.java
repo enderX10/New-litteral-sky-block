bcf94d03d800f789d09b0b85cdbd2b83fd3ca22c
enderX10 <enderx.xe@gmail.com>
2026-09-13 15:17:30 +0200

    Delete literal-sky-block-main directory

--- literal-sky-block-main/literal-sky-block-main/src/main/java/dev/latvian/mods/literalskyblock/client/CapturedInfo.java
+++
@@ -1,7 +0,0 @@
-package dev.latvian.mods.literalskyblock.client;
-
-import net.minecraft.client.renderer.LightTexture;
-import org.joml.Matrix4f;
-
-public record CapturedInfo(LightTexture lightTexture, Matrix4f frustumMatrix, Matrix4f projectionMatrix) {
-}
